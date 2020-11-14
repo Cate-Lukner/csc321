3dbc918e2add4980f6362a4fa8874eac0e306230
Cate-Lukner <46268240+Cate-Lukner@users.noreply.github.com>
2020-11-13 18:24:46 -0600

    Update hwserver.py

--- hw3/hwserver/hwserver.py
+++ hw3/hwserver/hwserver.py
@@ -9,7 +9,7 @@ import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
+socket.bind("tcp://node00:5555")
 
 while True:
     #  Wait for next request from client
